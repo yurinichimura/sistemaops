92bd3ddc94dbb4b5a9eb9d3493f5788e549013ff
Yuri Nichimura <139904028+yurinichimura@users.noreply.github.com>
2023-09-04 18:28:22 -0300

    Update stderr.c

--- stderr.c
+++ stderr.c
@@ -2,36 +2,18 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) {
+int main (int argc, char *argv[]) {
     pid_t childpid = 0;
-    int i, n = 100;
-
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s processes\n", argv[0]);
+    int i, n =100;
+    if (argc != 2){ /* check for valid number of command-line arguments */
+        fprintf(stderr, "Usage: %s processes\n", argv[0]); 
         return 1;
     }
-
     n = atoi(argv[1]);
-
-    for (i = 1; i < n; i++) {
-        if ((childpid = fork()) == -1) {
-            perror("fork"); 
-            return 1;
-        }
-
-        if (childpid == 0) {
-         
-            fprintf(stderr, "Child: i:%d process ID:%ld parent ID:%ld\n",
-                    i, (long)getpid(), (long)getppid());
+    for (i = 1; i < n; i++)
+        if (childpid = fork())
             break;
-        } else {
-            // 
-            fprintf(stderr, "Parent: i:%d process ID:%ld\n",
-                    i, (long)getpid());
-        }
-    }
-
+    fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
+        i, (long)getpid(), (long)getppid(), (long)childpid);
     return 0;
-}
-
-
+        }
