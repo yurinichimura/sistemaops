307e6a883d0c1661b2ba7d3ee4f2bedfe5885790
Yuri Nichimura <139904028+yurinichimura@users.noreply.github.com>
2023-09-04 18:18:42 -0300

    Update stderr.c

--- stderr.c
+++ stderr.c
@@ -2,24 +2,36 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main() {
+int main(int argc, char *argv[]) {
     pid_t childpid = 0;
-    int i, n = 100; 
+    int i, n = 100;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s processes\n", argv[0]);
+        return 1;
+    }
+
+    n = atoi(argv[1]);
 
     for (i = 1; i < n; i++) {
         if ((childpid = fork()) == -1) {
-            perror("Erro ao criar um processo filho");
+            perror("fork"); 
             return 1;
         }
 
-            if (childpid == 0) {
-      
-            fprintf(stderr, "i: %d ProcessID: %ld ParentID: %ld ChildID: %ld\n",
-                    i, (long)getpid(), (long)getppid(), (long)childpid);
+        if (childpid == 0) {
+         
+            fprintf(stderr, "Child: i:%d process ID:%ld parent ID:%ld\n",
+                    i, (long)getpid(), (long)getppid());
             break;
+        } else {
+            // 
+            fprintf(stderr, "Parent: i:%d process ID:%ld\n",
+                    i, (long)getpid());
         }
     }
 
     return 0;
 }
-# sistemaops
+
+
