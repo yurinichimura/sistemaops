f354450b7cb994253a3bbc76055b8e6a979ce6a9
Cloud User <ec2-user@ip-172-31-89-182.ec2.internal>
2023-09-04 20:21:13 +0000

    first commit

---
+++ stdout.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+    pid_t childpid = 0;
+    int i, n = 100; 
+
+    for (i = 1; i < n; i++) {
+        if ((childpid = fork()) == -1) {
+            perror("Erro ao criar um processo filho");
+            return 1;
+        }
+
+            if (childpid == 0) {
+      
+            fprintf(stdout, "i: %d ProcessID: %ld ParentID: %ld ChildID: %ld\n",
+                    i, (long)getpid(), (long)getppid(), (long)childpid);
+            break;
+        }
+    }
+
+    return 0;
+}
+# sistemaops
